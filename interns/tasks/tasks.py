7e0b45bd72a1700e33786c5b6de40919a97f56e4
brett <brett@debian-laptop>
2016-06-14 11:37:13 -0700

    Changed celery task app name to interns

--- interns/tasks/tasks.py
+++ interns/tasks/tasks.py
@@ -3,7 +3,8 @@ from interns.creds import config as creds_config
 from interns.clients.twitter import utils as twitter_utils
 
 from celery import Celery
-app = Celery(
+
+interns = Celery(
     'tasks',
     broker='amqp://{0}:{1}@{2}:{3}//'.format(
         creds_config.rabbit_uname,
@@ -14,12 +15,12 @@ app = Celery(
 )
 
 
-@app.task
+@interns.task
 def add(x, y):
     return x + y
 
 
-@app.task
+@interns.task
 def track_twitter_user(username):
    twitter_utils.begin_tracking_twitter_user(username)
 
