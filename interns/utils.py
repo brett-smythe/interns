84195e2849eb659e74bb7ac15064205bd2012d44
brett <brett@debian-laptop>
2016-07-09 15:49:53 -0700

    Attempting a different log conf name to avoid it getting purged

--- interns/utils.py
+++ interns/utils.py
@@ -13,7 +13,7 @@ def get_logger(module_name):
     """
     here = os.path.abspath(os.path.dirname(__file__))
     #logging_conf_path = '{0}/{1}'.format(here, 'settings/logging.conf')
-    logging_conf_path = '{0}/{1}'.format(here, 'settings/logging_conf')
+    logging_conf_path = '{0}/{1}'.format(here, 'settings/interns_lg.conf')
     logging.config.fileConfig(logging_conf_path)
     logging.Formatter.converter = time.gmtime
     logger = get_task_logger(module_name)
