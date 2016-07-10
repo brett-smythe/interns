5224d0e9e059067989accb2924bb01fead8347da
brett <brett@debian-laptop>
2016-07-09 22:47:06 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt') as inf:
 
 setup(
     name='interns',
-    version='0.1.2',
+    version='0.1.3',
     description='Worker to gather text sources for related projects',
     long_description=long_description,
     author='Brett Smythe',
