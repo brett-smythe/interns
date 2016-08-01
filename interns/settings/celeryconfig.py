1c41c4e4a54d75fdc5ae4beab12fa64765302f0c
brett <brett@debian-laptop>
2016-07-31 21:15:42 -0700

    Updated interns settings

--- interns/settings/celeryconfig.py
+++ interns/settings/celeryconfig.py
@@ -1,10 +1,23 @@
-from interns.creds import config as creds_config
+"""Config data for celery"""
 from interns.settings import interns_settings
+try:
+    from interns.creds import config as creds_config
+    rabbit_uname = creds_config.rabbit_uname
+    rabbit_pwd = creds_config.rabbit_pwd
+    rabbitmq_host = interns_settings.rabbitmq_host
+    rabbitmq_port = interns_settings.rabbitmq_port
+except ImportError:
+    import ConfigParser
+    config = ConfigParser.ConfigParser()
+    config.read('/etc/opt/interns/interns_auth.cfg')
+    rabbit_uname = config.get('RabbitMQ', 'rabbit_uname')
+    rabbit_pwd = config.get('RabbitMQ', 'rabbit_pwd')
+    rabbitmq_host = config.get('RabbitMQ', 'rabbit_host')
+    rabbitmq_port = config.get('RabbitMQ', 'rabbit_port')
 
 BROKER_URL = 'amqp://{0}:{1}@{2}:{3}//'.format(
-    creds_config.rabbit_uname,
-    creds_config.rabbit_pwd,
-    interns_settings.rabbitmq_host,
-    interns_settings.rabbitmq_port
+    rabbit_uname,
+    rabbit_pwd,
+    rabbitmq_host,
+    rabbitmq_port
 )
-
