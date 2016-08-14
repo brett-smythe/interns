64c6305c6df7df19a2a18eb07203af83c8801890
brett <brett@debian-laptop>
2016-08-14 13:17:34 -0700

    Added signal handling for supervisor

--- interns/main.py
+++ interns/main.py
@@ -2,6 +2,7 @@
 be sinle threaded and single instanced. If this is changed the limitation
 checks will also need to be re-written
 """
+import signal
 from time import sleep
 from multiprocessing import Process, Queue
 
@@ -13,30 +14,44 @@ from interns import utils
 logger = utils.get_scheduler_logger(__name__)
 
 
-def run_scheduler():
-    """
-    The main entry point for the interns scheduling service, starts and
-    maintains all of the various timing jobs
-    """
-    logger.info('Starting interns scheduler')
-    service_running = True
-    logging_queue = Queue()
-    workerLogger = utils.MultiProcessLogger(logging_queue, logger)
-    twitter_jobs_queue = Queue()
-    twitter_jobs_worker = Process(
-        target=jobs.TwitterJobs, args=(twitter_jobs_queue, logging_queue)
-    )
-    twitter_jobs_worker.daemon = True
-    twitter_jobs_worker.start()
-    try:
-        while service_running:
-            # Some call to check if the service should keep running
-            workerLogger.write_log_messages()
-            sleep(0.5)
-    finally:
-        logger.info('Shutting down interns scheduler')
-        twitter_jobs_queue.put(interns_settings.process_poison)
-        twitter_jobs_worker.join()
+class Scheduler(object):
+    """Class for running scheduled jobs"""
+
+    def __init__(self):
+        self.service_running = True
+        self.run_scheduler()
+
+    def signal_handler(self, signum, frame):
+        """Handle SIGTERM and SIGKILL"""
+        # pylint: disable=unused-argument
+        self.service_running = False
+
+    def run_scheduler(self):
+        """
+        The main entry point for the interns scheduling service, starts and
+        maintains all of the various timing jobs
+        """
+        logger.info('Starting interns scheduler')
+        logging_queue = Queue()
+        workerLogger = utils.MultiProcessLogger(logging_queue, logger)
+        twitter_jobs_queue = Queue()
+        twitter_jobs_worker = Process(
+            target=jobs.TwitterJobs, args=(twitter_jobs_queue, logging_queue)
+        )
+        twitter_jobs_worker.daemon = True
+        twitter_jobs_worker.start()
+        try:
+            while self.service_running:
+                # Check to see if the process was killed
+                signal.signal(signal.SIGTERM, self.signal_handler)
+                signal.signal(signal.SIGKILL, self.signal_handler)
+                # Some call to check if the service should keep running
+                workerLogger.write_log_messages()
+                sleep(0.5)
+        finally:
+            logger.info('Shutting down interns scheduler')
+            twitter_jobs_queue.put(interns_settings.process_poison)
+            twitter_jobs_worker.join()
 
 if __name__ == '__main__':
-    run_scheduler()
+    Scheduler()
